394a92f0b399baa63d956644fe9f79a50fde7a69
Otavio Rafael <rafael2104@gmail.com>
2022-08-10 22:55:11 -0300

    update some styles of the tables

--- src/components/Rifa.jsx
+++ src/components/Rifa.jsx
@@ -2,9 +2,8 @@ import ArrayNumbers, { separar } from '../services/ArrayNumbers';
 import '../styles/Rifa.css';
 
 export default function Rifa() {
-    let numbers = ArrayNumbers(0, 19);
+    let numbers = ArrayNumbers(0, 99);
     let numberAux = [];
-    //console.log(numbers);
 
     numbers.forEach(element => {
         let num = `${element}`;
@@ -18,49 +17,7 @@ export default function Rifa() {
         numberAux.push(num);
     });
 
-    //console.log(numberAux);
-
     let numberAux2 = separar(numberAux, 4)
-
-    // console.log(numberAux2);
-    // console.log(numberAux2.length)
-
-
-    // for (let i = 0; i < numberAux2.length; i++) {
-    //     document.write(
-    //         `
-    //             <div className="Rifa">
-    //                 <div className="tg-wrap">
-    //                     <table className="tg">
-    //                         <thead>
-    //                             <tr>
-    //                                 <th className="tg-0lax" colSpan="2">TÍTULO</th>
-    //                             </tr>
-    //                         </thead>
-    //                         <tbody>
-    //                             <tr>
-    //                                 <td className="tg-0lax">${numberAux2[i][0]}</td>
-    //                                 <td className="tg-0lax" rowSpan="4">TEXTO DA RIFA</td>
-    //                             </tr>
-    //                             <tr>
-    //                                 <td className="tg-0lax">${numberAux2[i][1]}</td>
-    //                             </tr>
-    //                             <tr>
-    //                                 <td className="tg-0lax">${numberAux2[i][2]}</td>
-    //                             </tr>
-    //                             <tr>
-    //                                 <td className="tg-0lax">${numberAux2[i][3]}</td>
-    //                             </tr>
-    //                             <tr>
-    //                                 <td className="tg-0lax" colSpan="2">rodapé</td>
-    //                             </tr>
-    //                         </tbody>
-    //                     </table>
-    //                 </div>
-    //             </div>
-    //         `
-    //     );
-    // }
 
     let arrTeste = [];
 
@@ -81,34 +38,33 @@ export default function Rifa() {
         return (
             <div key={index} className="tableBackground">
                 <div className="tg-wrap">
-                    <table className="tg">
+                    <table className="tg tableGeneral">
                         <thead>
                             <tr>
-                                <th className="tg-0lax" colSpan="2">TÍTULO</th>
+                                <th className="tg-0lax title" colSpan="2">TÍTULO</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td className="tg-0lax">{item.number1}</td>
-                                <td className="tg-0lax" rowSpan="4">TEXTO DA RIFA</td>
+                                <td className="tg-0lax numbers">{item.number1}</td>
+                                <td className="tg-0lax text" rowSpan="4">TEXTO DA RIFA</td>
                             </tr>
                             <tr>
-                                <td className="tg-0lax">{item.number2}</td>
+                                <td className="tg-0lax numbers">{item.number2}</td>
                             </tr>
                             <tr>
-                                <td className="tg-0lax">{item.number3}</td>
+                                <td className="tg-0lax numbers">{item.number3}</td>
                             </tr>
                             <tr>
-                                <td className="tg-0lax">{item.number4}</td>
+                                <td className="tg-0lax numbers">{item.number4}</td>
                             </tr>
                             <tr>
-                                <td className="tg-0lax" colSpan="2">rodapé</td>
+                                <td className="tg-0lax footer" colSpan="2">rodapé</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
-
         )
     })
 
@@ -116,38 +72,5 @@ export default function Rifa() {
         <div className="Rifa">
             {itemList}
         </div>
-        // numberAux2.forEach((item) => {
-        //     <BoxSx>
-        //         <div className="Rifa">
-        //             <div className="tg-wrap">
-        //                 <table className="tg table">
-        //                     <thead>
-        //                         <tr>
-        //                             <th className="tg-0lax" colSpan="2">TÍTULO</th>
-        //                         </tr>
-        //                     </thead>
-        //                     <tbody>
-        //                         <tr>
-        //                             <td className="tg-0lax">{item[0]}</td>
-        //                             <td className="tg-0lax" rowSpan="4">TEXTO DA RIFA</td>
-        //                         </tr>
-        //                         <tr>
-        //                             <td className="tg-0lax">{item[1]}</td>
-        //                         </tr>
-        //                         <tr>
-        //                             <td className="tg-0lax">{item[2]}</td>
-        //                         </tr>
-        //                         <tr>
-        //                             <td className="tg-0lax">{item[3]}</td>
-        //                         </tr>
-        //                         <tr>
-        //                             <td className="tg-0lax" colSpan="2">rodapé</td>
-        //                         </tr>
-        //                     </tbody>
-        //                 </table>
-        //             </div>
-        //         </div>
-        //     </BoxSx>
-        // })
     );
 }
